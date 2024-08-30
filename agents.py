27d8c74b3173c4d5df8245309c462233d9ebb30c
diegomac <dijavaji@gmail.com>
2024-08-30 18:26:44 -0500

    agrego llamadas a LLMs con tools

--- agents.py
+++ agents.py
@@ -1,6 +1,8 @@
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain_community.tools import WikipediaQueryRun
 from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 from typing import Type
 from langchain_core.tools import BaseTool
@@ -8,6 +10,8 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 
 #pip install langchain-community==0.2.14
 #pip install wikipedia==1.4.0
+modelAi = "gpt-4o-mini" #"gpt-3.5-turbo-0125"
+model = ChatOpenAI(model=modelAi)
 
 #Agentes
 #Ya sabemos hacer chatbots Ahora vamos un paso mas alla: si queremos que el chatbot pueda hacer cosas,
@@ -22,18 +26,19 @@ def wiki_tool(query):
     wikipedia_query = WikipediaQueryRun(api_wrapper=wikipedia_api_wrapper)
     #Esencialmente es una funcion con algunos metadatos para que el LLM la entienda
     #print(wikipedia_query.name, wikipedia_query.description, wikipedia_query.args_schema.schema())
-    return wikipedia_query.invoke({"query":query})
+    #return wikipedia_query.invoke({"query":query})
+    return wikipedia_query
 
 #Podemos hacer nuestras propias herramientas con anotaciones
 @tool
 def weather(city_name: str) -> str:
     """Obtiene el pronostico del tiempo para una ciudad."""
     match city_name.lower():
-        case 'santiago':
+        case 'quito':
             return 'cold'
-        case 'valparaiso':
+        case 'gya':
             return 'windy'
-        case 'concepcion':
+        case 'cuenca':
             return 'rainy'
         case _:
             return 'fair'
@@ -53,10 +58,28 @@ class CompoundInterestCalculator(BaseTool):
         return str(initial_amount*(1+rate/100)**n_periods)
 
 
-
 if __name__ == '__main__':
     print('PyCharm')
     #print(wiki_tool("hp lovecraft"))
-    print(weather.name, weather.description, weather.args_schema.schema())
+    wikipedia_query = wiki_tool("hp lovecraft")
+    #print(weather.name, weather.description, weather.args_schema.schema())
     compound_interest_calc = CompoundInterestCalculator()
-    print(compound_interest_calc.name, compound_interest_calc.description, compound_interest_calc.args_schema.schema())
+    #print(compound_interest_calc.name, compound_interest_calc.description, compound_interest_calc.args_schema.schema())
+    # Bien, sabemos hacer herramientas, pero queremos que un LLM las llame...
+    # Para eso basta con presentárselas, y el modelo puede decidir usarlas, i.e. producir un output que pide ejecutar una funcion/tool
+    tools = [wikipedia_query, weather, compound_interest_calc]
+    model_with_tools = model.bind_tools(tools)
+
+    messages = [
+        SystemMessage("You're a helpful assistant"),
+        HumanMessage("Como esta el clima en quito"),
+    ]
+    model_output = model_with_tools.invoke(messages)
+    print(model_output)
+
+    print(model_output.tool_calls) #Aqui la respuesta del modelo dice "necesito llamar a weather para responder"...
+    tool_message = weather.invoke(model_output.tool_calls[0])
+    print(tool_message) #llamemoslo y le damos el resultado al model de nuevo
+    messages.extend([model_output, tool_message])
+    final_answer = model_with_tools.invoke(messages)
+    print(final_answer.content)
